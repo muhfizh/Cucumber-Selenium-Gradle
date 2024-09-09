eb8b5242e66f9140b0cfdef5e678f8b4fd2d2243
muhfizh <muhfizhufi@gmail.com>
2024-09-09 16:22:08 +0700

    Git Push tambahan baru

--- src/test/java/com/muhfizh/Base.java
+++ src/test/java/com/muhfizh/Base.java
@@ -8,13 +8,12 @@ public class Base {
     protected static WebDriver driver;
 
     protected void getDriver() {
+        WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm=usage");
         options.addArguments("--remote-allow=origin=*");
         options.addArguments("--window-size=1920,1080");
-        options.addArguments("--headless");
-        WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(options);
     }
 
